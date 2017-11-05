3660f2f1f5b76f7735dcef1eb5dceb0f133779a1
Erwin de Vreugd <erwindevreugd@casema.nl>
2017-11-05 10:07:44 +0100

    Added Send-Event

---
+++ PSTruPortal/Public/Send-Event.ps1
@@ -0,0 +1,84 @@
+function Send-Event {
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "")]
+    param (
+        [Parameter(
+            Position=0, 
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [string]$Host = $Script:Host,
+
+        [Parameter(
+            Position=1, 
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [string]$SessionKey = $Script:SessionKey,
+
+        [Parameter(
+            Mandatory=$true
+        )]
+        [string]$Description,
+
+        [Parameter(
+            Mandatory=$false
+        )]
+        [string]$DeviceDescription,
+
+        [Parameter(
+            Mandatory=$false
+        )]
+        [string]$PersonDescription,
+
+        [Parameter(
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [switch]$UseSSL = $Script:UseSSL,
+
+        [Parameter(
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [switch]$IgnoreCertificateErrors = $Script:IgnoreCertificateErrors
+    )
+    
+    begin {
+        if($IgnoreCertificateErrors) {
+            [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+        }
+    }
+    
+    process {
+        $endPoint       = "api/events"
+        $method         = "POST"
+        $contentType    = "application/json"
+        $uri            = "http" + $(if($UseSSL) { "s" }) + "://$($Host)/$($endPoint)"
+
+        $headers = @{
+            Authorization=$SessionKey;
+        }
+        
+        $body = @{
+            "description"=$Description;
+            "deviceDescription"=$DeviceDescription;
+            "personDescription"=$PersonDescription;
+        }
+
+        Write-Verbose -Message "$($method) $($uri) $($contentType)"
+        Write-Verbose -Message "Body:`n$(ConvertTo-Json $body)"
+
+        $message = @{
+            Uri=$uri;
+            Method=$method;
+            Body=(ConvertTo-Json $body);
+            ContentType=$contentType;
+            Headers=$headers;
+            UseBasicParsing=$true;
+        }
+        Invoke-RestMethod @message | Out-Null
+    }
+    
+    end {
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $null }
+    }
+}
