5c7df2604283238bf44d64c5b490ea5d343d1e93
Erwin de Vreugd <erwindevreugd@casema.nl>
2017-11-13 07:59:29 +0100

    Split Get-SessionKeyFromScriptContext cmdlet

--- PSTruPortal/Public/Get-SessionKeyFromScriptContext.ps1
+++ PSTruPortal/Public/Get-SessionKeyFromScriptContext.ps1
@@ -9,12 +9,7 @@ function Get-SessionKeyFromScriptContext {
     process {
 
         $properties = @{
-            Host=$Script:Host;
-            Username=$Script:Username;
             SessionKey=$Script:SessionKey;
-            UseSSL=$Script:UseSSL;
-            IgnoreCertificateErrors=$Script:IgnoreCertificateErrors;
-            Timestamp=$Script:Timestamp;
         }
 
         New-Object -TypeName PSObject -Property $properties
