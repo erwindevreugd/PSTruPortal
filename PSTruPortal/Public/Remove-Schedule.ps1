e4b57425a6d8da5abd158b1a6b1c4198f22735bf
Erwin de Vreugd <erwindevreugd@casema.nl>
2017-11-13 09:24:54 +0100

    Added more cmdlets

---
+++ PSTruPortal/Public/Remove-Schedule.ps1
@@ -0,0 +1,75 @@
+function Remove-Schedule {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Position=0, 
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [string]$Host = $Script:Host,
+
+        [Parameter(
+            Position=1, 
+            Mandatory=$false,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [string]$SessionKey = $Script:SessionKey,
+
+        [Parameter(
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [switch]$UseSSL = $Script:UseSSL,
+
+        [Parameter(
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [switch]$IgnoreCertificateErrors = $Script:IgnoreCertificateErrors,
+        
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [int]$ScheduleId
+    )
+    
+    begin {
+        if($IgnoreCertificateErrors) {
+            [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+        }
+    }
+    
+    process {
+        $endPoint       = "api/schedules"
+        $method         = "DELETE"
+        $contentType    = "application/json"
+        $uri            = "http" + $(if($UseSSL) { "s" }) + "://$($Host)/$($endPoint)"
+
+        if($ScheduleId) {
+            $uri = "$uri/$ScheduleId"
+        }
+        
+        $headers = @{
+            Authorization=$SessionKey;
+        }
+        
+        $body = @{
+        }
+
+        Write-Verbose -Message "$($method) $($uri) $($contentType)"
+        Write-Verbose -Message "Body:`n$(ConvertTo-Json $body)"
+
+        $message = @{
+            Uri=$uri;
+            Method=$method;
+            Body=(ConvertTo-Json $body);
+            ContentType=$contentType;
+            Headers=$headers;
+            UseBasicParsing=$true;
+        }
+        Invoke-RestMethod @message | Out-Null
+    }
+    
+    end {
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $null }
+    }
+}
